4c89a29bd94cb9437bed52fed8d6dc102d2cab4e
Lucas <lucasfelipesantos2012@gmail.com>
2018-04-05 23:34:26 -0300

    Correcoes

--- frontend/src/containers/UserPetsListPage.js
+++ frontend/src/containers/UserPetsListPage.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { PetList } from '../components/pet/index';
 import { withRouter } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
 
 class UserPetsListPage extends React.Component {
 
   render() {
     let userId = this.props.match.id;
     return (
-      <PetList id={userId} />
+      <div className='addUserForm'>
+        <Grid
+          textAlign='center'
+          style={{ height: '100%' }}
+          verticalAlign='middle'
+        >
+          <PetList id={userId} />
+        </Grid>
+      </div>
     );
   }
 }
