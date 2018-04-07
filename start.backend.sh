45444658d9872425a7cf99a5cf9f6d1a867d7ed4
João Paulo Séder <jpfmscel@gmail.com>
2018-04-06 23:04:26 -0300

    logs na pasta graphql/logs/

--- start.backend.sh
+++ start.backend.sh
@@ -1,7 +1,8 @@
-echo "Ambiente : " $1
-if [ "$1" == "dev" ]; 
+if [ "$1" == "prod" ]; 
 then
-    cd graphql && npm install && npm run dev &
-else
+    echo "Ambiente : " $1
     cd graphql && npm install && npm start &
+else
+    echo "Ambiente : DEV"
+    cd graphql && npm install && npm run dev &
 fi
