984fa6ec259bd942cf998d330ed373c9f7eeb290
Gilluan <gilluan@hotmail.com.br>
2018-04-10 23:14:29 -0300

    Corrigindo o Select

--- frontend/src/shared/FormikSelect.js
+++ frontend/src/shared/FormikSelect.js
@@ -1,45 +1,41 @@
-import React from 'react';
-import { Form } from 'semantic-ui-react';
-import { Field } from "formik";
+import React from "react";
+import { render } from "react-dom";
+import { Field, Form as FormFormik } from "formik";
+import { withFormik } from "formik";
+import Yup from "yup";
+import { Form, Select } from "semantic-ui-react";
 
 const FormikSemanticSelect = ({
-  field: { name },
-  form: {touched, errors, setFieldTouched, setFieldValue},
+  field: { name, value },
+  form: { touched, errors, setFieldTouched, setFieldValue },
   options,
   onChange,
   ...props
 }) => {
   const handleChange = (event, key, data) => {
-    setFieldTouched(name, true);
-    setFieldValue(name, data);
+    setFieldTouched(name, true)
+    setFieldValue(name, key.value)
 
     if (onChange) {
-      onChange(event, key, data);
+      onChange(event, key, data)
     }
-  };
+  }
 
-return (
-  <Form.Field>
-      <Form.Select
+  return (
+    <Form.Field>
+      <Select
         {...props}
         name={name}
-        error={ !!touched[name] && !!errors[name] }
+        value={value}
         options={options}
         onChange={handleChange}
         onBlur={setFieldTouched}
       />
       {touched[name] && errors[name] && <div>{errors[name]}</div>}
-  </Form.Field>
-);
-};
+    </Form.Field>
+  )
+}
 
-const FormikSelect = (field, options, ...props) => (
-  <Field
-    {...field}
-    component={FormikSemanticSelect}
-    {...props}
-  >
-  </Field>
-);
+const FormikSelect = ({ field, ...props }) => (<Field {...field} {...props} component={FormikSemanticSelect} />)
 
 export default FormikSelect;
