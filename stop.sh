40248833f5b6f59a76df1a0131985621d5536eea
EliasOliveira <jun.oliveira10@gmail.com>
2018-04-12 13:40:58 -0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,27 +1,27 @@
 if [ "$1" == "prod" ]; 
 then
-	pidsYarn=`ps -ef | grep yarn | cut -d" " -f7`
+	pidsYarn=`ps -A | grep yarn | cut -d" " -f1`
 	for pid in $pidsYarn
 	do
 		echo 'Eliminando processo Yarn ' $pid
 		kill -9 $pid
 	done
 
-	pidsNode=`ps -ef | grep node | grep pets | cut -d" " -f7`
+	pidsNode=`ps -A | grep node | grep pets | cut -d" " -f1`
 	for pid in $pidsNode
 	do
 		echo 'Eliminando processo Node ' $pid
 		kill -9 $pid
 	done
 else
-	pidsYarn=`ps -ef | grep yarn | cut -d" " -f4`
+	pidsYarn=`ps -A | grep yarn | cut -d" " -f1`
 	for pid in $pidsYarn
 	do
 		echo 'Eliminando processo Yarn ' $pid
 		kill -9 $pid
 	done
 
-	pidsNode=`ps -ef | grep node | grep pets | cut -d" " -f4`
+	pidsNode=`ps -A | grep node | grep pets | cut -d" " -f1`
 	for pid in $pidsNode
 	do
 		echo 'Eliminando processo Node ' $pid
